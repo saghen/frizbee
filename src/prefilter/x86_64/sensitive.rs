b349630045d0fc2ef9308307676cae548b6bdb7d
Liam Dyer <liamcdyer@gmail.com>
2026-01-22 16:30:56 -0500

    docs: update prefilter safety

--- src/prefilter/x86_64/sensitive.rs
+++ src/prefilter/x86_64/sensitive.rs
@@ -5,13 +5,9 @@ use std::arch::x86_64::*;
 /// bytes. For example, if the needle is "test", the haystack "tset" will return true. However,
 /// the order does matter across 16 byte boundaries.
 ///
-/// Fastest with SSE2, AVX, and AVX2, but still very fast with just SSE2. Use a function with
-/// `#[target_feature(enable = "sse2,avx,avx2")]` or `#[target_feature(enable = "sse2")]`
-///
 /// # Safety
-/// When W > 16, the caller must ensure that the minimum length of the haystack is >= 16.
-/// When W <= 16, the caller must ensure that the minimum length of the haystack is >= 8.
-/// In all cases, the caller must ensure the needle.len() > 0 and that SSE2 is available.
+/// The caller must ensure that the minimum length of the haystack is >= 8.
+/// The caller must ensure the needle.len() > 0 and that SSE2 is available.
 #[inline(always)]
 pub unsafe fn match_haystack_unordered(needle: &[u8], haystack: &[u8]) -> (bool, usize) {
     let len = haystack.len();
