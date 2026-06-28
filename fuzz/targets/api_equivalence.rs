1b25f87455ef77ea41a90cb94aaebb5c0fa89fde
Liam Dyer <liamcdyer@gmail.com>
2026-06-28 14:09:46 -0400

    chore: drop remaining old fuzzing code

--- fuzz/targets/api_equivalence.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    bolero::check!()
-        .with_iterations(256)
-        .with_max_len(4096)
-        .for_each(|input: &[u8]| frizbee::fuzz_support::assert_public_api(input));
-}
--- fuzz/targets/prefilter.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    bolero::check!()
-        .with_iterations(8192)
-        .with_max_len(2048)
-        .for_each(|input: &[u8]| frizbee::fuzz_support::assert_prefilter(input));
-}
